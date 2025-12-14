2974dcd77381fd5f5d795fe8a022aa666d165f86
khaldi islam abdel fattah <khaldi.islamabdelfattah@univ-ouargla.dz>
2025-12-14 09:54:38 +0100

    Initial commit

---
+++ arr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int r;
+    char name[10];
+   char*b[]={"bachir","outhman","amin","ali"};
+   char*w[]={"mokhtar","islam","ahmed","ayad"};
+printf("enter your name");
+scanf("%s",&name);
+for(int i=0;i<4;i++){
+if( name=='b[i]'){
+r=0;}
+if('name'=='w[i]'){
+   r=1;}}
+if(r==1){
+    printf("welcom mr %s",name);}
+else
+    printf("nigger ");
+
+return 0;
+}
+
+
+
---
+++ e.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+ char*a="4";
+ char*b="5";
+int c= atoi(a)*atoi(b);
+printf("%d \n",c);
+return 0;
+}
